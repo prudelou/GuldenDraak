92330086e2df7b2e964e81ee46892d5ea4b9b072
Pierre Rudelou <pierre.rudelou@hotmail.fr>
2017-11-15 15:54:42 +0000

    add communication script

---
+++ communication/client.py
@@ -0,0 +1,14 @@
+﻿import socket
+
+hote = "localhost"
+port = 12800
+
+connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_avec_serveur.connect((hote, port))
+print("Connexion établie avec le serveur sur le port {}".format(port))
+
+msg_recu = connexion_avec_serveur.recv(1024)
+print(msg_recu)
+
+print("Fermeture de la connexion")
+connexion_avec_serveur.close()
---
+++ communication/server.py
@@ -0,0 +1,17 @@
+﻿import socket
+
+hote = ''
+port = 12800
+
+connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_principale.bind((hote, port))
+connexion_principale.listen(5)
+print("Le serveur écoute à présent sur le port {}".format(port))
+
+connexion_avec_client, infos_connexion = connexion_principale.accept()
+
+connexion_avec_client.send(b"Connexion accepte")
+
+print("Fermeture de la connexion")
+connexion_avec_client.close()
+connexion_principale.close()
